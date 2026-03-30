6090609a47b972605d2d491ca4ed6495de70e616
govind <goviton1073@gmail.com>
2026-03-30 22:10:41 +0530

    Initial clean ETL pipeline project

--- scriptss/.ipynb_checkpoints/load-checkpoint.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def load_data(df, path):
-    df.to_csv(path, index=False)
--- scriptss/extract.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def extract_data(path):
-    df = pd.read_csv(path, encoding='windows-1252')
-    return df
